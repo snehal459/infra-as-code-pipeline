89dfd92c14af30f24990d5ee446b031302e3f750
snehal459 <gadgesnehal27@gmail.com>
2026-04-19 15:13:31 +0530

    Completed networking module with VPC, subnet, IGW, route table

---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for public subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "AZ for subnet"
+  default     = "ap-south-1a"
+}

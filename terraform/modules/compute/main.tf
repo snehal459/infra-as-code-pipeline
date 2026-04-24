ab0bd261761496ad6c838dcd25610277ef7e0981
snehal459 <gadgesnehal27@gmail.com>
2026-04-24 12:09:31 +0530

    fixed ci cd pipeline

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -138,3 +138,34 @@ resource "aws_ecs_service" "service" {
     aws_lb_listener.listener
   ]
 }
+########################################
+# AUTO SCALING TARGET
+########################################
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 6
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+########################################
+# AUTO SCALING POLICY (CPU BASED)
+########################################
+resource "aws_appautoscaling_policy" "cpu_policy" {
+  name               = "cpu-scaling-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value       = 50.0   # scale if CPU > 50%
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
+
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+  }
+}
